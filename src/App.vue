7524fbbf4c05b49905bfc44080112caf3a94743d
alfred-moyo <alf.moxz@gmail.com>
2024-12-23 23:33:24 +0400

    updated .vue and .css

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,44 @@
-<template>  
-  <div class="background"></div>
-  <header>
-    <div class="logo">
-      ANGA Hub
-      <!-- <img src="@/assets/logo.png" alt="Logo"> -->
-    </div>
-    <button class="menu-toggle" @click="toggleMenu">
-      <span class="menu-icon">☰</span>
-    </button>
-    <nav :class="{ open: isMenuOpen }">
-      <ul  @click="toggleMenu">
-        <li><router-link to="/">
+<template>
+  <div id="app" class="container">
+    <header>
+      <div class="logo">LOGO</div>
+      <div class="menu-toggle" @click="toggleMenu">
+        <div v-if="!menuOpen" class="menu-icon">☰</div>
+        <div v-else class="close">✕</div>
+      </div>
+      <nav :class="{ open: menuOpen }">
+        <ul @click="toggleMenu">
+          <li>
+            <router-link to="/">
               <span>
-                <img width="30" src="@/assets/images/home.png" alt="home icon"/> HOME
+                <img width="30" src="@/assets/images/home.png" alt="home icon" /> HOME
               </span>
-            </router-link></li>
-        <li><router-link to="/about">              
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/about">
               <span>
-                <img width="30" src="@/assets/images/about.png" alt="about icon"/> ABOUT
+                <img width="30" src="@/assets/images/about.png" alt="about icon" /> ABOUT
               </span>
-            </router-link></li>
-        <li><router-link to="/services">
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/services">
               <span>
-                <img width="30" src="@/assets/images/services.png" alt="service icon"/> SERVICES
+                <img width="30" src="@/assets/images/services.png" alt="service icon" /> SERVICES
               </span>
-            </router-link></li>
-        <li><router-link to="/contact">
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/contact">
               <span>
-                <img width="30" src="@/assets/images/contact.png" alt="contact icon"/> CONTACT
+                <img width="30" src="@/assets/images/contact.png" alt="contact icon" /> CONTACT
               </span>
-            </router-link></li>
-      </ul>
-    </nav>
-  </header>
-  <router-view></router-view>
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <router-view></router-view>
+  </div>
 </template>
-
-<script>
-
-  export default {
-    data() {
-      return {
-        isMenuOpen: false,
-      };
-    },
-    methods: {
-      toggleMenu() {
-        this.isMenuOpen = !this.isMenuOpen;
-      },
-    },
-  };
-
-</script>
-
-<!-- <style>
-  #app {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-</style> -->
---
+++ src/components/new.vue
@@ -0,0 +1,63 @@
+<template>  
+  <div class="background"></div>
+  <header>
+    <div class="logo">
+      ANGA Hub
+      <!-- <img src="@/assets/logo.png" alt="Logo"> -->
+    </div>
+    <button class="menu-toggle" @click="toggleMenu">
+      <span class="menu-icon">☰</span>
+    </button>
+    <nav :class="{ open: isMenuOpen }">
+      <ul  @click="toggleMenu">
+        <li><router-link to="/">
+              <span>
+                <img width="30" src="@/assets/images/home.png" alt="home icon"/> HOME
+              </span>
+            </router-link></li>
+        <li><router-link to="/about">              
+              <span>
+                <img width="30" src="@/assets/images/about.png" alt="about icon"/> ABOUT
+              </span>
+            </router-link></li>
+        <li><router-link to="/services">
+              <span>
+                <img width="30" src="@/assets/images/services.png" alt="service icon"/> SERVICES
+              </span>
+            </router-link></li>
+        <li><router-link to="/contact">
+              <span>
+                <img width="30" src="@/assets/images/contact.png" alt="contact icon"/> CONTACT
+              </span>
+            </router-link></li>
+      </ul>
+    </nav>
+  </header>
+  <router-view></router-view>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        isMenuOpen: false,
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+      },
+    },
+  };
+
+</script>
+
+<!-- <style>
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style> -->
