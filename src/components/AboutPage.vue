9705b0555557de4ed4030b3f985634d1daa7f7a7
alfred-moyo <alf.moxz@gmail.com>
2024-12-29 00:11:09 +0400

    updated aboutpage ,vue & css

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -2,7 +2,6 @@
     <router-view>
         <main>
             <h1>About Us</h1>
-            <div class="underline"></div>
             <p>At ANGA Hub LLC, we are driven by a shared vision to lead Africa into the next generation of innovation, excellence, and impactful solutions.
                 The name "ANGA" represents "Africa NextGen Apex," reflecting our commitment to future-oriented innovation and achieving the highest standards in everything we do.</p>
             <h2>Mission</h2>
@@ -11,8 +10,10 @@
                 <li>To expand into various sectors, creating a diverse portfolio of companies under the ANGA brand.</li>
                 <li>To prioritize sustainability, social impact, and excellence in all operations.</li>
             </ul>
-            <h2 id="vision">Vision</h2>
-            <p class="vision">To be a leading conglomerate that drives innovation, solves challenges, and creates value across multiple sectors in Africa and beyond.</p>
+            <div class="vision">
+                <h2>Vision</h2>
+                <p class="vision">To be a leading conglomerate that drives innovation, solves challenges, and creates value across multiple sectors in Africa and beyond.</p>               
+            </div>
             <h2>Core Values</h2>
             <div class="corevalues">
                 <div class="circle">
@@ -47,7 +48,6 @@
                 </div>
             </div>
         </main>
-        <h1>{{ msg }}</h1>
     </router-view>
 </template>
 
