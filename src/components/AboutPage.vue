cc8bb622282567f214c093c74e3affdbe371bade
alfred-moyo <alf.moxz@gmail.com>
2024-12-28 21:25:02 +0400

    updated the aboutus page

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -2,6 +2,7 @@
     <router-view>
         <main>
             <h1>About Us</h1>
+            <div class="underline"></div>
             <p>At ANGA Hub LLC, we are driven by a shared vision to lead Africa into the next generation of innovation, excellence, and impactful solutions.
                 The name "ANGA" represents "Africa NextGen Apex," reflecting our commitment to future-oriented innovation and achieving the highest standards in everything we do.</p>
             <h2>Mission</h2>
@@ -14,31 +15,31 @@
             <p class="vision">To be a leading conglomerate that drives innovation, solves challenges, and creates value across multiple sectors in Africa and beyond.</p>
             <h2>Core Values</h2>
             <div class="corevalues">
-                <div>
+                <div class="circle">
                     <span>
                         <img src="@/assets/images/innovation.png" alt="innovation">
                     </span>
                     <h3>Innovation</h3>
                 </div>
-                <div>
+                <div class="circle">
                     <span>
                         <img src="@/assets/images/integrity.png" alt="integrity">
                     </span>
                     <h3>Integrity</h3>
                 </div>
-                <div>
+                <div class="circle">
                     <span>
                         <img src="@/assets/images/social-impact.png" alt="social-impact">
                     </span>
                     <h3>Social Impact</h3>
                 </div>
-                <div>
+                <div class="circle">
                     <span>
                         <img src="@/assets/images/excellence.png" alt="excellence">
                     </span>
                     <h3>Excellence</h3>
                 </div>
-                <div>
+                <div class="circle">
                     <span>
                         <img src="@/assets/images/sustainability.png" alt="sustainability">
                     </span>
@@ -51,12 +52,12 @@
 </template>
 
 <script>
-    export default {
-        name: 'AboutPage',
-        props: {
-            msg: String,
-        },
-    };
+export default {
+    name: 'AboutPage',
+    props: {
+        msg: String,
+    },
+};
 </script>
 
 <style scoped src="@/assets/styles/aboutpage.css"></style>
