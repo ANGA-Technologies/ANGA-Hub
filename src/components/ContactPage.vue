679365c320ffa1931b3b5bd9739637f3b91a6797
alfred-moyo <alf.moxz@gmail.com>
2024-12-30 00:47:13 +0400

    updated contactpage

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -1,19 +1,25 @@
 <template>
     <router-view>
         <main>
-            <h1>ANGA</h1>
-            <h2>HUB</h2>
-            <h3>Empowering Africa with Next-Gen Solutions</h3>
+            <h1>Contact Us</h1>
+            <div class="contact_info">
+                <h2>Contact Information</h2>
+                <h3>Say something to start a live chat!</h3>
+
+            </div>
         </main>
     </router-view>
 </template>
     
 <!-- <script>    
     export default {
-        name: 'HomePage',
+        name: 'ContactPage',
         props: {
         msg: String
         }
     }
 </script> -->
+
+<style scoped src="@/assets/styles/contactpage.css"></style>
+
     
