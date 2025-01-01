29feb94bb2d17b1753934741c9ed1999d55b1108
EdwinAbdonShayo <contact@edwinshayo.com>
2025-01-01 20:52:10 +0400

    Enhance ServicePage styles; add smooth scrolling, custom scrollbar, and layout adjustments for improved usability and aesthetics

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -1,23 +1,78 @@
 <template>
-    <router-view>
-        <main>
-            <h1>Contact Us</h1>
-            <div class="contact_info">
+    <main>
+        <h1>Contact Us</h1>
+        <div class="content">
+            <div class="contacts">
                 <h2>Contact Information</h2>
-                <h3>Say something to start a live chat!</h3>
-
+                <ul>
+                    <li><span><img src="@/assets/images/call.png" /></span> <a href="tel:+23058187657"> +230 5818 7657 </a></li>
+                    <li><span><img src="@/assets/images/mail.png" /></span><a href="mailto:info@angahub.com"> info@angahub.com</a></li>
+                    <li><span><img src="@/assets/images/location.png" /></span> Port Louis, Mauritius </li>
+                </ul>
+                <div class="links">
+                    <a href="https://x.com/angahub" target="_blank"><img src="@/assets/images/x.png" /></a>
+                    <a href="https://www.linkedin.com/company/angahub" target="_blank"><img src="@/assets/images/linkedin.png" /></a>
+                    <a href="" target="_blank"><img src="@/assets/images/whatsapp.png" /></a>
+                </div>
             </div>
-        </main>
-    </router-view>
+            <div class="forms">
+                <h3>Any question or remarks? Just write us a message!</h3>
+                <form>
+                    <div class="horizontal-group">
+                        <div class="input-group">
+                            <label for="fname">First Name</label>
+                            <input type="text" id="fname" placeholder="Happy" required autocomplete="given-name" />
+                            
+                            <label for="lname">Last Name</label>
+                            <input type="text" id="lname" placeholder="Doe" required autocomplete="family-name" />
+                        </div>
+                        <div class="input-group">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" placeholder="happy@doe.com" required autocomplete="email" />
+                            
+                            <label for="phone-number">Phone Number</label>
+                            <input type="text" id="phone-number" placeholder="+1 012 3456 789" autocomplete="tel" />
+                        </div>
+                    </div>
+                    
+                    <div class="input-group">
+                        <fieldset>
+                            <legend>Select Subject?</legend>
+                            <div class="radio-group">
+                                <label class="radio-option">
+                                    <input type="radio" name="subject" value="general" checked autocomplete="off">
+                                    General Inquiry
+                                </label>
+                                <label class="radio-option">
+                                    <input type="radio" name="subject" value="pricing" autocomplete="off">
+                                    Pricing Inquiry
+                                </label>
+                                <label class="radio-option">
+                                    <input type="radio" name="subject" value="company" autocomplete="off">
+                                    Company Inquiry
+                                </label>
+                                <label class="radio-option">
+                                    <input type="radio" name="subject" value="tech" autocomplete="off">
+                                    Technical Inquiry
+                                </label>
+                            </div>
+                        </fieldset>
+                    </div>
+                    
+                    <div class="input-group">
+                        <label for="message">Message</label>
+                        <textarea id="message" placeholder="Write your message.." autocomplete="off"></textarea>
+                    </div>
+                    
+                    <button type="submit">Send Message</button>
+                </form>
+            </div>
+        </div>
+    </main>
 </template>
     
 <!-- <script>    
-    export default {
-        name: 'ContactPage',
-        props: {
-        msg: String
-        }
-    }
+
 </script> -->
 
 <style scoped src="@/assets/styles/contactpage.css"></style>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const webpack = require('webpack');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true, // Set to false if you're not using the Options API
+        __VUE_PROD_DEVTOOLS__: false, // Set to true if you need devtools in production
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false // This resolves the warning
+      })
+    ]
+  }
+};
